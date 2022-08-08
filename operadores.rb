95e77c1bd78a1216df79e052ade1e47b43346f65
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-08-08 10:14:24 -0300

    Curso Ruby QA Ninja

---
+++ operadores.rb
@@ -0,0 +1,41 @@
+
+
+#numero1 = 0
+#numero2 = 0
+#total = 0
+
+#puts 'Informe o número 1'
+#numero1 = gets.chomp.to_i
+
+#puts 'Tnforme o número 2'
+#numero2 = gets.chomp.to_i
+
+#total = numero1 + numero2
+#puts 'A soma é: ', total
+
+#total = numero1 - numero2
+#puts 'A subtração é: ', total
+
+#total = numero1 * numero2
+#puts 'A multiplicação é: ', total
+
+#total = numero1 / numero2
+#puts 'A divisão é: ', total
+
+# total = numero1 + numero2
+# puts 'A soma é: ', total
+
+# OPERADORES DE COMPARAÇÃO:
+v1 = 12
+v2 = 11
+
+# maior que >
+# menor que <
+# maior ou igual >=
+# menor ou igual <=
+# diferente !=
+# igual ==
+# igual eql?
+
+puts v1 > v2
+puts v1.eql?(v2)
