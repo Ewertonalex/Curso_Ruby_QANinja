188d86acf8a3cab7289023c853f2e25b77effe0a
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-08-09 15:12:15 -0300

    construtor

--- Ruby Avançado/classes.rb
+++ Ruby Avançado/classes.rb
@@ -3,6 +3,9 @@
 class Conta
     attr_accessor :saldo
 
+    def initialize
+        self.saldo = 0.0
+
     def deposita(valor)
         #puts 'Depositando a quantia de ' + valor.to_s + ' reais.'
         self.saldo += valor
@@ -12,6 +15,5 @@ end
 
 c = Conta.new
 
-c.saldo = 0.0
 c.deposita(100.00)
 puts c.saldo
