41da18c0656f68b991e602c10f2f84fced9ec127
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-08-10 13:42:32 -0300

    classes

--- Ruby Avançado/classes.rb
+++ Ruby Avançado/classes.rb
@@ -1,19 +1,25 @@
 
 
 class Conta
-    attr_accessor :saldo
+    attr_accessor :saldo, :nome
 
-    def initialize
+    def initialize(nome)
         self.saldo = 0.0
+        self.nome = nome
+    end
 
     def deposita(valor)
         #puts 'Depositando a quantia de ' + valor.to_s + ' reais.'
         self.saldo += valor
-        puts "Depositando a quantia de #{valor} reais."
+        puts "Depositando a quantia de #{valor} reais, na conta de #{self.nome}."
     end
 end
 
-c = Conta.new
+c = Conta.new('Ewerton')
 
 c.deposita(100.00)
 puts c.saldo
+
+c.deposita(10.00)
+puts c.saldo
+puts c.nome
