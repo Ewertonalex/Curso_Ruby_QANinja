1ce44832edd9247ea79534a4bfff2a1f311d41b9
Ewerton Alexander <91163104+Ewertonalex@users.noreply.github.com>
2022-08-11 14:52:49 -0300

    tratando exceccoes

--- Ruby Avançado/excecoes.rb
+++ Ruby Avançado/excecoes.rb
@@ -1,10 +1,22 @@
+#ler o arquivo e se tiver algum erro ira imprimir a exceção
 
+# begin
+#     # Devo tentar alguma coisa
+#     file = File.open('./ola') #devia ser ola.txt
+#     if file
+#         puts file.read
+#     end
+# rescue Exception => e
+#     # Obter um possível erro
+#     puts e.message #mensagem do erro
+#     puts e.backtrace #traça o caminho que fez e montra onde ta o erro
+# end
 
-begin
-    # Devo tentar alguma coisa
-    file = File.open('./ola')
-
-rescue
-    # Obter um possível erro
-
+def soma(n1, n2)
+    n1 + n2
+rescue Exception => e
+    #puts e.message #imprimir msg do erro
+    puts 'Erro ao executar a soma!' #possivel customizar a mensagem do erro.
 end
+
+soma('10', 5) #erro string + inteiro
